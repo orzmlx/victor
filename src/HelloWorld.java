25d38c7ff936ee45806d74c48010849c28f61053
orzmlx <43385093@qq.com>
2019-06-16 22:27:50 +0800

    Author:victor
    Signed-off-by: orzmlx <43385093@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello China");
         System.out.println("Hello Germany");
+        System.out.println("Hello mei");
     }
 }
